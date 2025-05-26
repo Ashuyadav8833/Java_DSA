477e8cfdc2e4732421e275c8a44703efb752a955
Ashuyadav8833 <ashu8833yadav@gmail.com>
2025-05-26 19:18:08 +0530

    Question 1

---
+++ max_min_element.java
@@ -0,0 +1,17 @@
+class max_min_element {
+    public static void main(String[] args) {
+        int arr[] = {21,11,28,34,6,9,69};
+        int max,min;
+        max = min = arr[0];
+
+        for (int i = 0; i < arr.length; i++) {
+            if(max < arr[i])
+                max = arr[i];
+            if(min > arr[i])
+                min = arr[i];
+        }
+
+        System.out.println("Max element is : " + max);
+        System.out.println("Min element is : " + min);
+    }
+}
